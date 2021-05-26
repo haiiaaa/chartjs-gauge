71775f9b62ec025f6ea183ec1583c6f190978592
Jan Bartels <jan.bartels@siempelkamp.com>
2021-05-26 14:24:53 +0200

    Initial rewrite for Chartjs 3

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
 import Chart from 'chart.js';
+import GaugeController from './controller';
 
-import GaugeController from './controllers/controller.gauge';
-
-import polyfill from './polyfill';
-
-polyfill();
-
-Chart.controllers.gauge = GaugeController;
-Chart.Gauge = (context, config) => {
-  config.type = 'gauge';
-
-  return new Chart(context, config);
-};
-
-export default Chart.Gauge;
+Chart.register(GaugeController);
